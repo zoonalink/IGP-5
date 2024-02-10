0f0b3b751b4233d84857c5bc90dbb161f54cb354
zoonalink <zoonalink@gmail.com>
2024-02-10 23:03:56 +0000

    base classifiers models and code

--- code/pl-functions.py
+++ code/pl-functions.py
@@ -1,20 +1,3 @@
-def resample_data(df, freq, agg_func='mean'):
-    """
-    Resamples the given DataFrame based on the specified frequency and aggregation function.
-
-    Parameters:
-    - df (DataFrame): The input DataFrame.
-    - freq (str): The frequency at which to resample the dat. (e.g., H for hourly, D for daily, 5T for every 5 minutes),
-    - agg_func (str, optional): The aggregation function to apply during resampling. Defaults to 'mean'.
-
-    Returns:
-    - df_resampled (DataFrame): The resampled DataFrame.
-    """
-    df_resampled = df.set_index('timestamp').groupby('id').resample(freq).agg(agg_func)
-    df_resampled.reset_index(inplace=True)
-    return df_resampled
-
-
 def extract_folder(folderpath, add_scores=False, downsample=None):
     """
     Extract CSV data from folder and subfolders into a dataframe.
@@ -94,4 +77,98 @@ def extract_folder(folderpath, add_scores=False, downsample=None):
     df.drop('condition', axis=1, inplace=True)
 
     # Final concat
+    return df
+
+
+def extract_full_days(df):
+    """
+    Extracts the records from the dataframe equal to full days,
+    (where each day has exactly 1440 records).
+
+    Parameters:
+    df (pandas.DataFrame): The input dataframe containing timestamp and id columns.
+
+    Returns:
+    pandas.DataFrame: The subset of the input dataframe that contains only the records
+    corresponding to full days.
+    """
+    # Convert timestamp to date
+    df['date'] = df['timestamp'].dt.date
+
+    # Count the number of records per day
+    counts = df.groupby(['id', 'date']).size()
+
+    # Get the dates that have 1440 records
+    full_days = counts[counts == 1440].reset_index()['date']
+
+    # Extract only the records for these dates
+    df_full_days = df[df['date'].isin(full_days)]
+
+    return df_full_days
+
+
+
+def resample_data(df, freq, agg_func='mean'):
+    """
+    Resamples the given DataFrame based on the specified frequency and aggregation function.
+
+    Parameters:
+    - df (DataFrame): The input DataFrame.
+    - freq (str): The frequency at which to resample the dat. (e.g., H for hourly, D for daily, 5T for every 5 minutes),
+    - agg_func (str, optional): The aggregation function to apply during resampling. Defaults to 'mean'.
+
+    Returns:
+    - df_resampled (DataFrame): The resampled DataFrame.
+    """
+    df_resampled = df.set_index('timestamp').groupby('id').resample(freq).agg(agg_func)
+    df_resampled.reset_index(inplace=True)
+    return df_resampled
+
+
+def extract_full_days(df):
+    """
+    Extracts the records from the input DataFrame that correspond to full days, 
+    where each day has exactly 1440 records.
+
+    Parameters:
+    df (DataFrame): The input DataFrame containing timestamp and id columns.
+
+    Returns:
+    DataFrame: The subset of the input DataFrame that contains only the records 
+    corresponding to full days.
+    """
+   
+    # Convert timestamp to date
+    df['date'] = df['timestamp'].dt.date
+
+    # Count the number of records per day
+    counts = df.groupby(['id', 'date']).size()
+
+    # Get the dates that have 1440 records
+    full_days = counts[counts == 1440].reset_index()['date']
+
+    # Extract only the records for these dates
+    df_full_days = df[df['date'].isin(full_days)]
+
+    return df_full_days
+
+
+
+
+
+def normalise_data(df, columns_to_normalise):
+    """
+    Normalise the specified columns in the df using StandardScaler.
+
+    Parameters:
+    - df (pandas.DataFrame): The DataFrame to be normalised.
+    - columns_to_normalise (list): A list of column names to be normalised.
+
+    Returns:
+    - df (pandas.DataFrame): The DataFrame with the specified columns normalised.
+    """
+    from sklearn.preprocessing import StandardScaler
+
+    scaler = StandardScaler()
+    df[columns_to_normalise] = scaler.fit_transform(df[columns_to_normalise])
     return df
